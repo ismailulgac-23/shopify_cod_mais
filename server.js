575de2e1bb9a626aaad7302ddcea10d941affa75
ismailulgac-23 <82471789+ismailulgac-23@users.noreply.github.com>
2026-01-21 03:43:51 +0300

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = 3002;
+const port = 3003;
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
