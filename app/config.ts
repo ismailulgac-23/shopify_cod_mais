3522dd2a24d25d108caa4f68f949e97591ce1e8c
ismailulgac-23 <82471789+ismailulgac-23@users.noreply.github.com>
2026-01-21 03:51:12 +0300

    first commit

--- app/config.ts
+++ app/config.ts
@@ -1,11 +1,4 @@
 export const getEnv = (shop: any = undefined) => {
-  if (shop == "m2gtzh-yv.myshopify.com") {
-    return {
-      key: process.env.SHOPIFY_API_KEY_MAIS,
-      secret: process.env.SHOPIFY_API_SECRET_MAIS,
-    };
-  }
-
   return {
     key: process.env.SHOPIFY_API_KEY,
     secret: process.env.SHOPIFY_API_SECRET,
